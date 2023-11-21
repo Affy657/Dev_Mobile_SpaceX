34c005e5a0207230726cd8c2b64ec96d1aef99a2
shaynlink <guemmoudrayane03@gmail.com>
2023-11-21 16:16:57 +0100

    adding linter cleaning code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,97 +1,99 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
-import { useEffect, useState } from 'react';
-import { useColorScheme } from 'react-native';
-import { OnBoard } from '../components/Onboard';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { useFonts } from 'expo-font'
+import { SplashScreen, Stack } from 'expo-router'
+import React, { useEffect, useState } from 'react'
+import { useColorScheme } from 'react-native'
+import { OnBoard } from '../components/Onboard'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export {
   // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
+  ErrorBoundary
+} from 'expo-router'
 
-export const unstable_settings = {
+export const unstableSettings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
+  initialRouteName: '(tabs)'
+}
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 type ReadyState = 'FONT_LOADED' | 'FIRST_TIME_INFO_LOADED'
 const ALL_READY_STATE: ReadyState[] = ['FONT_LOADED', 'FIRST_TIME_INFO_LOADED']
 
 // Uncomment to reset AsyncStorage.
-//AsyncStorage.clear();
+// AsyncStorage.clear();
 
-function checkAllList(list: ReadyState[], readyState: ReadyState[]) {
+/**
+ * @return {boolean}
+ */
+function checkAllList (list: ReadyState[], readyState: ReadyState[]): boolean {
   return list.every((item) => readyState.includes(item))
 }
 
-export default function RootLayout() {
-  const [firstTime, setFirstTime] = useState(true);
-  const [readyState, setReadyState] = useState<ReadyState[]>([]);
+export default function RootLayout (): React.ReactNode {
+  const [firstTime, setFirstTime] = useState(true)
+  const [readyState, setReadyState] = useState<ReadyState[]>([])
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
+    ...FontAwesome.font
+  })
 
   const isFirstTimeConnected = async (): Promise<boolean> => {
-    const alreadyLogged = await AsyncStorage.getItem('alreadyLogged');
+    const alreadyLogged = await AsyncStorage.getItem('alreadyLogged')
 
-    if (!alreadyLogged) {
+    if (alreadyLogged === undefined) {
       return true
     }
 
     return alreadyLogged !== 'true'
   }
 
-  const handleStart = async () => {
-    setFirstTime(false);
-    await AsyncStorage.setItem('alreadyLogged', 'true');
+  const handleStart = (): void => {
+    setFirstTime(false)
+    void AsyncStorage.setItem('alreadyLogged', 'true')
   }
 
   // // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+    if (error !== null) throw error
+  }, [error])
 
   useEffect(() => {
     if (loaded) {
-      setReadyState(['FONT_LOADED', ...readyState]);
+      setReadyState(['FONT_LOADED', ...readyState])
     }
-  }, [loaded]);
+  }, [loaded])
 
   useEffect(() => {
     if (checkAllList(ALL_READY_STATE, readyState)) {
-      SplashScreen.hideAsync();
+      SplashScreen.hideAsync()
     }
   })
 
   useEffect(() => {
-    isFirstTimeConnected()
+    void isFirstTimeConnected()
       .then((firstTimeInfo) => {
-        setFirstTime(firstTimeInfo);
-        setReadyState(['FIRST_TIME_INFO_LOADED', ...readyState]);
+        setFirstTime(firstTimeInfo)
+        setReadyState(['FIRST_TIME_INFO_LOADED', ...readyState])
       })
   }, [firstTime])
 
   if (!checkAllList(ALL_READY_STATE, readyState)) {
-    return null;
+    return null
   }
 
   if (firstTime) {
-    return <OnBoard onStart={handleStart} />;
+    return <OnBoard onStart={handleStart} />
   }
 
-  return <RootLayoutNav />;
+  return <RootLayoutNav />
 }
 
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+function RootLayoutNav (): React.ReactNode {
+  const colorScheme = useColorScheme()
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
@@ -100,5 +102,5 @@ function RootLayoutNav() {
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
     </ThemeProvider>
-  );
+  )
 }
